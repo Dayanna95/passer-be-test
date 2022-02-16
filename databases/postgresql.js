ff1db335963ee4fe284e2eb1b80d8c31dbbc8547
Dayanna95 <dayannapadilla95@gmail.com>
2022-02-15 18:04:38 -0600

    table TRANSACTION

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -6,7 +6,9 @@ const postgresql = newDb();
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
                 insert into users values (123, 'Juan', true);`);
 
-
+//table transaction
+postgresql.public.none(`create table transaction(pk_transaction integer,fk_user integer, description text, amount decimal);
+                insert into transaction values (1,123,'pago internet',19.234);`);
 
 module.exports = {
     postgresql
